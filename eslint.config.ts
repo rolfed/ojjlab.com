aac1e359124238727f3ba0fe900befeefbea1c34
Danniel Rolfe <danniel.rolfe@experian.com>
2025-08-15 12:53:07 -0700

    Setup eslint

---
+++ eslint.config.ts
@@ -0,0 +1,28 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
+  },
+  globalIgnores([
+    "node_modules/*",
+    "dist/*",
+    "prettier.config.ts",
+    "eslint.config.ts",
+  ]),
+]);
+
