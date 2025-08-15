abd4f0ebb51a5e106a4a7846820dec829beba203
Danniel Rolfe <danniel.rolfe@experian.com>
2025-08-15 16:52:26 -0700

    Fix tailwindcss and vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   server: {
@@ -7,4 +8,7 @@ export default defineConfig({
   preview: {
     open: true,
   },
+  plugins: [
+    tailwindcss()
+  ]
 });
