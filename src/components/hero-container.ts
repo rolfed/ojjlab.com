2b555ba0ebfc30455c43330e1ae701d510c63a88
Danniel Rolfe <danniel.rolfe@experian.com>
2025-08-15 16:52:49 -0700

    Configure hero container

---
+++ src/components/hero-container.ts
@@ -0,0 +1,34 @@
+class HeroContainer extends HTMLElement {
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+  public connectedCallback() {
+    this.render();
+  }
+
+  public render(): void {
+    this.shadowRoot!.innerHTML = `
+      <section 
+        class="
+          relative
+          h-screen
+          flex
+          flex-col
+          justify-center
+          items-center
+          text-white
+          bg-black
+          px-6
+        "
+      >
+        <h1>Test</h1>
+        <p>Hello there want to become an amazing developer</p>
+      </section>
+    `;
+
+  }
+}
+
+customElements.define("hero-container", HeroContainer);
