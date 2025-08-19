87bc97cf1f54e7e5e2daaa646a1a0adcce520e10
Danniel Rolfe <talkto@dannielrolfe.com>
2025-08-19 10:18:17 -0700

    WIP - Creating layout

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-// import './style.css';
-// import typescriptLogo from './typescript.svg';
-// import viteLogo from '/vite.svg';
-import "./components/nav-container";
-import "./components/hero-container";
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <header>
-    <nav-container></nav-container>
-  </header>
-  <main>
-    <hero-container></hero-container>
-  </main>
-  <footer>
-  </footer>
-`;
+// // import './style.css';
+// // import typescriptLogo from './typescript.svg';
+// // import viteLogo from '/vite.svg';
+// import "./components/nav-container";
+// import "./components/hero-container";
+//
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <!-- <header> -->
+//   <!--   <nav-container></nav-container> -->
+//   <!-- </header> -->
+//   <!-- <main> -->
+//   <!--   <hero-container></hero-container> -->
+//   <!-- </main> -->
+//   <!-- <footer> -->
+//   <!-- </footer> -->
+// `;
