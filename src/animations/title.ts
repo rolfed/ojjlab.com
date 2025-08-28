1d4c30ad3227e60ae4180d5666a073072399b156
Danniel Rolfe <danniel.rolfe@experian.com>
2025-08-28 10:21:02 -0700

    Text animation is almost dialed

--- src/animations/title.ts
+++ src/animations/title.ts
@@ -2,41 +2,53 @@ import gsap from 'gsap';
 import { getAnimationSelector } from './data-animation';
 
 export interface TitleSelectors {
+  containerSelector: string;
   barSelector: string;
   textSelector: string;
 };
 
 const titleAnimation = (elements: TitleSelectors) => {
-  const { barSelector, textSelector } = elements;
+  const { containerSelector, barSelector, textSelector } = elements;
+  const container = document.querySelector(containerSelector)
   const bar = document.querySelector(barSelector);
   const text = document.querySelector(textSelector);
+
+  if (!container) {
+    console.warn('Title Animation: container element not found');
+    return;
+  }
 
   if (!text) {
     console.warn('Title Animation: text element not found');
     return;
   }
 
-  const tl = gsap.timeline();
+
+  const containerWidth = container.getBoundingClientRect().width.toString();
+  console.log('test: ', containerWidth);
+  const tl = gsap.timeline({
+    defaults: { ease: 'power2.out' }
+  });
 
   tl.to(
-    bar, { duration: 0.05, backgroundColor: "#ffffff", ease: "power1.inOut" }
-  ).to(
-      bar, { duration: 0.1, width: "100%", ease: "power2.out" }
-  ).to(
-      bar, { duration: 0.1, opacity: 0, onComplete: () => {
-        (text as HTMLElement).style.visibility = "visible";
-      }
-  });
+    bar, { x: 0, duration: 0.25, delay: 0.15 },
+  )
+  .to(
+    bar, { x: containerWidth, opacity: 0,  width: "7rem" }
+  )
+  .to(text, { opacity: "100%", duration: 0.25 });
 };
 
 export const heroTitleAnimation = () => {
 
-  const textSelector = getAnimationSelector("hero-title");
-  const barSelector = getAnimationSelector("hero-reveal-bar");
+  const containerSelector = getAnimationSelector('hero-reveal-container');
+  const textSelector = getAnimationSelector('hero-title');
+  const barSelector = getAnimationSelector('hero-reveal-bar');
 
   const heroTitle: TitleSelectors = {
-      barSelector,
-      textSelector
+    containerSelector,
+    barSelector,
+    textSelector
   };
 
   return titleAnimation(heroTitle);
