d780e0401b9ff525cecb7dba8888752dc3707a20
Danniel Rolfe <talkto@dannielrolfe.com>
2025-09-06 23:29:18 -0700

    wip

--- src/animations/hero.ts
+++ src/animations/hero.ts
@@ -20,40 +20,17 @@ export const heroAnimation = (doc: Document): Timeline => {
     return tl;
   }
 
-  // setHeroTransformDefaults('heroAnimation', ['hero-logo', 'hero-title-cta', 'hero-content-cta'])
-
   /* Animations */
   tl
     .addLabel("Hero")
     .add(barAnimation(revealBarElement, containerWidth), "start")
     .add(logoInAnimation(logoElement), "start+=0.5")
     .add(logoOutAnimation(logoElement), "start+=2")
-    .add(titleAnimation(location), "start+=2.15")
-    .add(contentAnimation(location), "start+=4");
+    .add(titleAnimation(location), "start+=3")
+    .add(contentAnimation(location), "start+=6.5")
+    .add(heroEnterLayout(location), "start+=10");
 
   return tl;
-};
-
-const setHeroTransformDefaults = (
-    location: string, 
-    elements: Array<string>,
-    vars: gsap.TweenVars = {}
-): Array<HTMLElement> => {
-    const _elements = elements
-        .map((key) => getElementByDataAnimation<HTMLElement>(location, key))
-        .filter((element): element is HTMLElement => !!element)
-
-    if (_elements.length) {
-        gsap.set(_elements, {
-            transformOrigin: "50% 50%",
-            willChange: "transfrom,opacity",
-            ...vars
-        });
-    } else {
-        console.warn("[setHeroTransofrmDefaults] No elements resolved for", elements)
-    }
-
-    return _elements;
 };
 
 const barAnimation = (element: HTMLElement, containerWidth: number): Timeline => {
@@ -78,42 +55,77 @@ const logoInAnimation = (element: HTMLElement): Timeline => {
 
 const logoOutAnimation = (element: HTMLElement): Timeline => {
   const tl = gsap.timeline({});
-  return tl.to(element, { autoAlpha: 0, scale: 0.95, duration: 0.25 });
+  return tl.to(element, { autoAlpha: 0, scale: 2, y: -16, duration: 0.75 });
+};
+
+const _textAnimation = (children: Array<HTMLElement>): Timeline => {
+    const tl = gsap.timeline({});
+
+    if (!children?.length) { return tl; }
+
+    tl.to(children, {
+        keyframes: [
+            { autoAlpha: 1, y: 0, scale: 1.5, duration: 0.5, ease: "power2.in" }, // show
+            { scale: 1, duration: 0.5, ease: "back.out(1.6)" },
+            { duration: 1, delay: 0.2 },
+            { autoAlpha: 0, scale: 2, y: -5, duration: 0.3, ease: "power2.in" } // disapear
+        ],
+        stagger: { each: 0.35, from: "start" }
+    });
+
+    return tl;
 };
 
 const titleAnimation = (location: string): Timeline => {
-  const tl = gsap.timeline({});
-  const titleElement = getElementByDataAnimation<HTMLElement>(location, "hero-title-cta");
-
-  //gsap.set(titleElement, { y: -50 });
-
-  const children = titleElement?.querySelectorAll('div');
-
-  return tl.to(children, {
-    opacity: "100%",
-    //y: -100,
-    duration: 0.75,
-    scale: 1.5,
-    ease: "power1.in",
-    stagger: 0.75
-  }).to(titleElement, { autoAlpha: 0 });
-  
-}
+    const titleElement = getElementByDataAnimation<HTMLElement>(location, "hero-title-cta");
+    gsap.set(titleElement, { opacity: "100%" });
+    const children = titleElement?.querySelectorAll('div');
+    return _textAnimation(children);
+};
 
 const contentAnimation = (location: string): Timeline => {
-  const tl = gsap.timeline({});
   const contentElement = getElementByDataAnimation<HTMLElement>(location, "hero-content-cta");
   const children = contentElement?.querySelectorAll('span');
-
-  // gsap.set(contentElement, { y: -75 });
-
-  return tl.to(children, {
-    opacity: "100%",
-    scale: 1.5,
-    // y: -100,
-    duration: 0.75,
-    ease: "power1.in",
-    stagger: 0.75
-  });
+  return _textAnimation(children);
 };
+
+const heroEnterLayout = (location: string): Timeline => {
+    const tl = gsap.timeline({});
+    const hero = getElementByDataAnimation<HTMLElement>(location, "hero");
+    const root = getElementByDataAnimation<HTMLElement>(location, "root");
+    const heroContent = getElementByDataAnimation<HTMLElement>(location, "hero-content");
+    const nav = getElementByDataAnimation<HTMLElement>(location, "nav");
+    const container = getElementByDataAnimation<HTMLElement>(location, "hero-overlay")
+    const overlayContent = getElementByDataAnimation<HTMLElement>(location, "hero-overlay-content");
+    const revealBar = getElementByDataAnimation<HTMLElement>(location, "hero-reveal-bar");
+
+    if (!hero 
+        || !root 
+        || !container 
+        || !nav 
+        || !overlayContent
+        || !revealBar
+    ) { return tl; }
+
+    const getHeight = window.innerWidth >= 768 ? "70vh" : "100dvh";
+
+    tl.to(hero, {
+        ["height"]: () => getHeight,
+        duration: 0.7,
+        ease: "power2.inOut"
+    }).add(() => {
+        container.style.setProperty('height', getHeight);
+        container.style.setProperty('min-height', getHeight);
+        nav.removeAttribute('hidden');
+        revealBar.classList.add('hidden');
+        overlayContent.classList.add('hidden');
+        hero.classList.add("hero-entered");
+        root.classList.remove("overflow-hidden");
+    }).to([heroContent, nav], {
+        opacity: "100%",
+        visibility: "visible" 
+    });
+
+    return tl;
+}
 
